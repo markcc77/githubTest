9699ab9bceaead0baf95533f907c77c4b4840765
markcc77 <70893134+markcc77@users.noreply.github.com>
2020-10-09 00:18:28 +0800

    update

--- navbarMod/navbarMod.js
+++ navbarMod/navbarMod.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
